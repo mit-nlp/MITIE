a389dec14c9caf39287a293cffc67c7532537856
Davis E. King <davis.king@ll.mit.edu>
2014-02-07 14:46:31 -0500

    refactored NER code a bit

--- tools/ner/src/main.cpp
+++ tools/ner/src/main.cpp
@@ -10,6 +10,7 @@
 #include <dlib/svm_threaded.h>
 #include <mitie/total_word_feature_extractor.h>
 #include <mitie/stemmer.h>
+#include <mitie/unigram_tokenizer.h>
 
 
 
@@ -23,6 +24,7 @@ void train_chunker(const command_line_parser& parser);
 void test_chunker(const command_line_parser& parser);
 void train_id(const command_line_parser& parser);
 void test_id(const command_line_parser& parser);
+void tag_file(const command_line_parser& parser);
 
 // ----------------------------------------------------------------------------------------
 
@@ -41,6 +43,8 @@ int main(int argc, char** argv)
         parser.add_option("threads", "Use <arg> threads when doing training (default: 4).",1);
         parser.add_option("cache-size", "Set the max cutting plane cache size to <arg> (default: 5).",1);
 
+        parser.add_option("tag-file", "Read in a text file and tag it with the ner model in file <arg>.",1);
+
         parser.parse(argc,argv);
         parser.check_option_arg_range("C", 1e-9, 1e9);
         parser.check_option_arg_range("threads", 1, 64);
@@ -54,6 +58,12 @@ int main(int argc, char** argv)
         {
             cout << "Usage: ner [options]\n";
             parser.print_options(); 
+            return 0;
+        }
+
+        if (parser.option("tag-file"))
+        {
+            tag_file(parser);
             return 0;
         }
 
@@ -154,6 +164,14 @@ std::vector<matrix<float,0,1> > sentence_to_feats (
 
 // ----------------------------------------------------------------------------------------
 
+std::string get_mitie_models_path()
+{
+    const char* models = getenv("MITIE_MODELS");
+    if (models==0)
+        throw dlib::error("MITIE_MODELS environment variable not set.  It should contain the path to the MITIE-models repository.");
+    return models;
+}
+
 void train_chunker(const command_line_parser& parser)
 {
     std::vector<std::vector<std::string> > sentences;
@@ -162,8 +180,10 @@ void train_chunker(const command_line_parser& parser)
     parse_conll_data(parser[0], sentences, chunks, chunk_labels);
     cout << "number of sentences loaded: "<< sentences.size() << endl;
 
+    const std::string models_path = get_mitie_models_path();
+
     total_word_feature_extractor fe;
-    std::ifstream fin("total_word_feature_extractor.dat", ios::binary);
+    std::ifstream fin((models_path + "/total_word_feature_extractor.dat").c_str(), ios::binary);
     deserialize(fe, fin);
 
     cout << "words in dictionary: " << fe.get_num_words_in_dictionary() << endl;
@@ -385,13 +405,11 @@ inline std::pair<uint64,uint64> caps_pattern (
 typedef std::vector<std::pair<dlib::uint32,double> > sample_type;
 
 sample_type extract_chunk_features (
-    const std::vector<std::vector<std::string> >& sentences,
-    const unsigned long idx,
+    const std::vector<std::string>& words,
     const std::vector<matrix<float,0,1> >& sentence,
     const std::pair<unsigned long, unsigned long>& pos
 )
 {
-    const std::vector<std::string>& words = sentences[idx];
     DLIB_CASSERT(words.size() == sentence.size(), "range can't be empty");
     DLIB_CASSERT(pos.first != pos.second, "range can't be empty");
 
@@ -573,6 +591,94 @@ unsigned long get_label (
 
 // ----------------------------------------------------------------------------------------
 
+namespace mitie
+{
+    class named_entity_extractor
+    {
+        /*!
+            WHAT THIS OBJECT REPRESENTS
+        !*/
+    public:
+
+        named_entity_extractor(){}
+
+        named_entity_extractor(
+            const std::map<unsigned long, std::string>& possible_tags_,
+            const total_word_feature_extractor& fe_,
+            const dlib::sequence_segmenter<ner_feature_extractor>& segmenter_,
+            const dlib::multiclass_linear_decision_function<dlib::sparse_linear_kernel<sample_type>,unsigned long>& df_
+        ) : possible_tags(possible_tags_), fe(fe_), segmenter(segmenter_), df(df_) 
+        {
+            DLIB_CASSERT(df.number_of_classes() == possible_tags.size()+1,"invalid inputs");
+        }
+
+        void operator() (
+            const std::vector<std::string>& sentence,
+            std::vector<std::pair<unsigned long, unsigned long> >& chunks,
+            std::vector<unsigned long>& chunk_tags
+        ) const
+        {
+            const std::vector<matrix<float,0,1> >& sent = sentence_to_feats(fe, sentence);
+            segmenter.segment_sequence(sent, chunks);
+
+            const unsigned long NOT_AN_ENTITY = df.number_of_classes()-1;
+
+            chunk_tags.resize(chunks.size());
+            // now label each chunk
+            for (unsigned long j = 0; j < chunks.size(); )
+            {
+                chunk_tags[j] = df(extract_chunk_features(sentence, sent, chunks[j]));
+
+                // if this chunk is predicted to not be an entity then erase it
+                if (chunk_tags[j] == NOT_AN_ENTITY)
+                {
+                    std::swap(chunk_tags[j], chunk_tags.back());
+                    std::swap(chunks[j], chunks.back());
+                    chunk_tags.resize(chunk_tags.size()-1);
+                    chunks.resize(chunks.size()-1);
+                }
+                else
+                {
+                    ++j;
+                }
+            }
+        }
+
+        const std::map<unsigned long, std::string>& get_possible_tags (
+        ) const { return possible_tags; }
+
+        friend void serialize(const named_entity_extractor& item, std::ostream& out)
+        {
+            int version = 1;
+            dlib::serialize(version, out);
+            dlib::serialize(item.possible_tags, out);
+            serialize(item.fe, out);
+            serialize(item.segmenter, out);
+            serialize(item.df, out);
+        }
+
+        friend void deserialize(named_entity_extractor& item, std::istream& in)
+        {
+            int version = 0;
+            dlib::deserialize(version, in);
+            if (version != 1)
+                throw dlib::serialization_error("Unexpected version found while deserializing mitie::named_entity_extractor.");
+            dlib::deserialize(item.possible_tags, in);
+            deserialize(item.fe, in);
+            deserialize(item.segmenter, in);
+            deserialize(item.df, in);
+        }
+
+    private:
+        std::map<unsigned long, std::string> possible_tags;
+        total_word_feature_extractor fe;
+        dlib::sequence_segmenter<ner_feature_extractor> segmenter;
+        dlib::multiclass_linear_decision_function<dlib::sparse_linear_kernel<sample_type>,unsigned long> df;
+    };
+}
+
+// ----------------------------------------------------------------------------------------
+
 void train_id(const command_line_parser& parser)
 {
     std::vector<std::vector<std::string> > sentences;
@@ -589,7 +695,7 @@ void train_id(const command_line_parser& parser)
     deserialize(segmenter, fin);
 
     std::vector<sample_type> samples;
-    std::vector<double> labels;
+    std::vector<unsigned long> labels;
     for (unsigned long i = 0; i < sentences.size(); ++i)
     {
         const std::vector<matrix<float,0,1> >& sent = sentence_to_feats(fe, sentences[i]);
@@ -607,7 +713,7 @@ void train_id(const command_line_parser& parser)
         std::set<std::pair<unsigned long,unsigned long> >::const_iterator j;
         for (j = ranges.begin(); j != ranges.end(); ++j)
         {
-            samples.push_back(extract_chunk_features(sentences, i, sent, *j));
+            samples.push_back(extract_chunk_features(sentences[i], sent, *j));
             labels.push_back(get_label(chunks[i], chunk_labels[i], *j));
         }
     }
@@ -615,7 +721,7 @@ void train_id(const command_line_parser& parser)
     cout << "now do training" << endl;
     cout << "num training samples: " << samples.size() << endl;
 
-    svm_multiclass_linear_trainer<sparse_linear_kernel<sample_type> > trainer;
+    svm_multiclass_linear_trainer<sparse_linear_kernel<sample_type>,unsigned long> trainer;
 
     const double C = get_option(parser, "C", 450.0);
     const double eps = get_option(parser, "eps", 0.001);
@@ -635,7 +741,7 @@ void train_id(const command_line_parser& parser)
     cout << "overall accuracy: "<< sum(diag(res))/sum(res) << endl;
     */
 
-    multiclass_linear_decision_function<sparse_linear_kernel<sample_type> > df;
+    multiclass_linear_decision_function<sparse_linear_kernel<sample_type>,unsigned long> df;
     df = trainer.train(samples, labels);
     matrix<double> res = test_multiclass_decision_function(df, samples, labels);
     cout << "test on train: \n" << res << endl;
@@ -645,17 +751,23 @@ void train_id(const command_line_parser& parser)
     cout << "epsilon:     "<< eps << endl;
     cout << "num_threads: "<< num_threads << endl;
 
-    ofstream fout("ner_chunk_id_classifier.dat", ios::binary);
-    serialize(df, fout);
+    std::map<unsigned long,std::string> ner_labels;
+    ner_labels[PER] = "PERSON";
+    ner_labels[LOC] = "LOCATION";
+    ner_labels[ORG] = "ORGANIZATION";
+    ner_labels[MISC] = "MISC";
+    named_entity_extractor ner(ner_labels, fe, segmenter, df);
+    ofstream fout("ner_model.dat", ios::binary);
+    serialize(ner, fout);
 }
 
 // ----------------------------------------------------------------------------------------
 
 void test_id(const command_line_parser& parser)
 {
-    multiclass_linear_decision_function<sparse_linear_kernel<sample_type> > df;
-    ifstream fin("ner_chunk_id_classifier.dat", ios::binary);
-    deserialize(df, fin);
+    named_entity_extractor ner;
+    ifstream fin("ner_model.dat", ios::binary);
+    deserialize(ner, fin);
 
     std::vector<std::vector<std::string> > sentences;
     std::vector<std::vector<std::pair<unsigned long, unsigned long> > > chunks;
@@ -663,51 +775,26 @@ void test_id(const command_line_parser& parser)
     parse_conll_data(parser[0], sentences, chunks, chunk_labels);
     cout << "number of sentences loaded: "<< sentences.size() << endl;
 
-    fin.close();
-    fin.open("trained_segmenter.dat", ios::binary);
-    total_word_feature_extractor fe;
-    sequence_segmenter<ner_feature_extractor> segmenter;
-    deserialize(fe, fin);
-    deserialize(segmenter, fin);
 
-    // find out how many in and out vocab words there are in the testing data
-    std::vector<std::string> temp = fe.get_words_in_dictionary();
-    std::set<std::string> vocab(temp.begin(), temp.end());
-    double oov = 0, iv = 0;
-    for (unsigned long i = 0; i < sentences.size(); ++i)
-    {
-        for (unsigned long j = 0; j < sentences[i].size(); ++j)
-        {
-            if (vocab.count(sentences[i][j]) != 0)
-                ++iv;
-            else
-                ++oov;
-        }
-    }
-    cout << "iv:  " << iv << endl;
-    cout << "oov: " << oov << endl;
-    cout << "iv/oov: " << iv/oov << endl;
+    const unsigned long num_labels = ner.get_possible_tags().size();
+    std::vector<double> num_targets(num_labels);
+    std::vector<double> num_dets(num_labels);
+    std::vector<double> num_true_dets(num_labels);
 
-
-    std::vector<double> num_targets(4);
-    std::vector<double> num_dets(4);
-    std::vector<double> num_true_dets(4);
+    std::vector<std::pair<unsigned long, unsigned long> > ranges;
+    std::vector<unsigned long> predicted_labels;
 
     for (unsigned long i = 0; i < sentences.size(); ++i)
     {
-        const std::vector<matrix<float,0,1> >& sent = sentence_to_feats(fe, sentences[i]);
-
-        std::vector<std::pair<unsigned long, unsigned long> > ranges;
-        ranges = segmenter(sent);
+        ner(sentences[i], ranges, predicted_labels);
 
         for (unsigned long j = 0; j < ranges.size(); ++j)
         {
-            const unsigned long predicted_label = (unsigned long)df(extract_chunk_features(sentences, i, sent, ranges[j]));
+            const unsigned long predicted_label = predicted_labels[j];
             const unsigned long true_label = get_label(chunks[i], chunk_labels[i], ranges[j]);
 
-            if (predicted_label != NOT_ENTITY)
-                num_dets[predicted_label]++;
-            if (predicted_label == true_label && true_label != NOT_ENTITY)
+            num_dets[predicted_label]++;
+            if (predicted_label == true_label)
                 num_true_dets[true_label]++;
         }
         for (unsigned long j = 0; j < chunk_labels[i].size(); ++j)
@@ -734,6 +821,47 @@ void test_id(const command_line_parser& parser)
     cout << "   precision: "<< prec << endl;
     cout << "   recall:    "<< recall << endl;
     cout << "   f1:        "<< 2*prec*recall/(prec+recall) << endl;
+
+}
+
+// ----------------------------------------------------------------------------------------
+
+void tag_file(const command_line_parser& parser)
+{
+    string ner_model = parser.option("tag-file").argument();
+    ifstream fin(ner_model.c_str(), ios::binary);
+    named_entity_extractor ner;
+    deserialize(ner, fin);
+
+    fin.close();
+    fin.open(parser[0].c_str());
+
+    unigram_tokenizer tok(fin);
+
+    std::vector<std::string> words;
+    string word;
+    while(tok(word))
+        words.push_back(word);
+
+    std::vector<std::pair<unsigned long, unsigned long> > ranges;
+    std::vector<unsigned long> predicted_labels;
+    std::map<unsigned long, std::string> tags = ner.get_possible_tags();
+    tags[tags.size()] = "O";
+    ner(words, ranges, predicted_labels);
+
+    std::vector<unsigned long> word_tags(words.size(), tags.size()-1);
+    for (unsigned long i = 0; i < ranges.size(); ++i)
+    {
+        for (unsigned long j = ranges[i].first; j < ranges[i].second; ++j)
+        {
+            word_tags[j] = predicted_labels[i];
+        }
+    }
+
+    for (unsigned long i = 0; i < words.size(); ++i)
+    {
+        cout << words[i] << "/" << tags[word_tags[i]] << " ";
+    }
 
 }
 
