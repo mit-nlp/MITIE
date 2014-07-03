8ef2f42d982cbd5963fbfc61ac22d078fbb91f98
Davis E. King <davis.king@ll.mit.edu>
2014-07-03 16:00:29 -0400

    Added a C++ API for training binary relation detectors.

---
+++ mitielib/include/mitie/binary_relation_detector_trainer.h
@@ -0,0 +1,206 @@
+// Copyright (C) 2014 Massachusetts Institute of Technology, Lincoln Laboratory
+// License: Boost Software License   See LICENSE.txt for the full license.
+// Authors: Davis E. King (davis.king@ll.mit.edu)
+#ifndef MIT_LL_BINARY_rELATION_DETECTION_TRAINER_H__
+#define MIT_LL_BINARY_rELATION_DETECTION_TRAINER_H__
+
+#include <mitie/binary_relation_detector.h>
+#include <mitie/named_entity_extractor.h>
+#include <mitie.h>
+
+namespace mitie
+{
+    class binary_relation_detector_trainer 
+    {
+        /*!
+            WHAT THIS OBJECT REPRESENTS
+                This is a tool for training mitie::binary_relation_detector objects from a
+                set of annotated training data.
+        !*/
+    public:
+        binary_relation_detector_trainer (
+            const std::string& relation_name,
+            const named_entity_extractor& ner
+        );
+        /*!
+            ensures
+                - #get_beta() == 0.1
+                - #num_threads() == 4
+                - A copy of the given ner object is used during the training process.
+                  Therefore, ner should be trained for the same language as the data we are
+                  going to use to train a binary relation detector. (e.g. Don't use a ner
+                  object for Spanish if you are trying to make an English language relation
+                  detector).
+                - #get_relation_name() == relation_name
+        !*/
+
+        std::string get_relation_name(
+        ) const;
+        /*!
+            ensures
+                - returns the name we will give to any binary_relation_detector objects
+                  created by this object.  That is, this string is used to populate the
+                  binary_relation_detector::relation_type field.
+        !*/
+
+        unsigned long num_positive_examples(
+        ) const;
+        /*!
+            ensures
+                - returns the number of positive training instances that have been added
+                  into this object.
+        !*/
+
+        unsigned long num_negative_examples(
+        ) const;
+        /*!
+            ensures
+                - returns the number of negative training instances that have been added
+                  into this object.
+        !*/
+
+        void add_positive_binary_relation (
+            const std::vector<std::string>& tokens,
+            unsigned long arg1_start,
+            unsigned long arg1_length,
+            unsigned long arg2_start,
+            unsigned long arg2_length
+        );
+        /*!
+            requires
+                - arg1_length > 0
+                - arg2_length > 0
+                - The arg indices reference valid elements of the tokens array.  That is,
+                  the following expressions should be valid:
+                    - tokens[arg1_start]
+                    - tokens[arg1_start+arg1_length-1]
+                    - tokens[arg2_start]
+                    - tokens[arg2_start+arg1_length-1]
+                - mitie_entities_overlap(arg1_start,arg1_length,arg2_start,arg2_length) == 0
+            ensures
+                - This function adds a positive training instance into the trainer.  That
+                  is, this function tells the trainer that the given tokens contain an
+                  example of the binary relation we are trying to learn.  Moreover, the
+                  first argument of the relation is located at tokens[arg1_start] and is
+                  arg1_length tokens long.  Similarly, the second argument of the relation
+                  is located at tokens[arg2_start] and is arg2_length tokens long.
+                - #num_positive_examples() == num_positive_examples() + 1
+        !*/
+
+        void add_positive_binary_relation (
+            const std::vector<std::string>& tokens,
+            const std::pair<unsigned long, unsigned long>& arg1,
+            const std::pair<unsigned long, unsigned long>& arg2
+        ) { add_positive_binary_relation(tokens, arg1.first, arg1.second-arg1.first, arg2.first, arg2.second-arg2.first); }
+
+        void add_negative_binary_relation (
+            const std::vector<std::string>& tokens,
+            unsigned long arg1_start,
+            unsigned long arg1_length,
+            unsigned long arg2_start,
+            unsigned long arg2_length
+        );
+        /*!
+            requires
+                - arg1_length > 0
+                - arg2_length > 0
+                - The arg indices reference valid elements of the tokens array.  That is,
+                  the following expressions should be valid:
+                    - tokens[arg1_start]
+                    - tokens[arg1_start+arg1_length-1]
+                    - tokens[arg2_start]
+                    - tokens[arg2_start+arg1_length-1]
+                - mitie_entities_overlap(arg1_start,arg1_length,arg2_start,arg2_length) == 0
+            ensures
+                - This function adds a negative training instance into the trainer.  That
+                  is, this function tells the trainer that the given tokens and argument
+                  combination is not a binary relation we are trying to learn.  The argument 
+                  indices have the same interpretation as they do for add_positive_binary_relation(). 
+                - #num_negative_examples() == num_negative_examples() + 1
+        !*/
+
+        void add_negative_binary_relation (
+            const std::vector<std::string>& tokens,
+            const std::pair<unsigned long, unsigned long>& arg1,
+            const std::pair<unsigned long, unsigned long>& arg2
+        ) { add_negative_binary_relation(tokens, arg1.first, arg1.second-arg1.first, arg2.first, arg2.second-arg2.first); }
+
+        unsigned long get_num_threads (
+        ) const;
+        /*!
+            ensures
+                - returns the number of threads that will be used to perform training.  You 
+                  should set this equal to the number of processing cores you have on your 
+                  computer.  
+        !*/
+
+        void set_num_threads (
+            unsigned long num
+        );
+        /*!
+            ensures
+                - #get_num_threads() == num
+        !*/
+
+        double get_beta (
+        ) const;
+        /*!
+            ensures
+                - returns the trainer's beta parameter.  This parameter controls the
+                  trade-off between trying to avoid false alarms but also detecting
+                  everything.  Different values of beta have the following interpretations:
+                    - beta < 1 indicates that you care more about avoiding false alarms
+                      than missing detections.  The smaller you make beta the more the
+                      trainer will try to avoid false alarms.
+                    - beta == 1 indicates that you don't have a preference between avoiding
+                      false alarms or not missing detections.  That is, you care about
+                      these two things equally.
+                    - beta > 1 indicates that care more about not missing detections than
+                      avoiding false alarms.
+        !*/
+
+        void set_beta (
+            double new_beta
+        );
+        /*!
+            requires
+                - new_beta >= 0
+            ensures
+                - #get_beta() == new_beta
+        !*/
+
+        binary_relation_detector train (
+        ) const;
+        /*!
+            requires
+                - num_positive_examples() > 0
+                - num_negative_examples() > 0
+            ensures
+                - Trains a binary_relation_detector based on the training instances given
+                  to this object via add_positive_binary_relation() and
+                  add_negative_binary_relation() calls and returns the result.
+                - The relation_type field of the returned object will be set to
+                  get_relation_name().
+        !*/
+
+    private:
+
+        /*!
+            CONVENTION
+                - pos_sentences.size() == pos_arg1s.size() == pos_arg2s.size()
+                - neg_sentences.size() == neg_arg1s.size() == neg_arg2s.size()
+        !*/
+        total_word_feature_extractor tfe;
+        double beta;
+        unsigned long num_threads;
+        std::string relation_name;
+        typedef std::pair<unsigned long, unsigned long> range;
+        std::vector<std::vector<std::string> > pos_sentences;
+        std::vector<range> pos_arg1s, pos_arg2s;
+        std::vector<std::vector<std::string> > neg_sentences;
+        std::vector<range> neg_arg1s, neg_arg2s;
+    };
+}
+
+#endif // MIT_LL_BINARY_rELATION_DETECTION_TRAINER_H__
+
---
+++ mitielib/src/binary_relation_detector_trainer.cpp
@@ -0,0 +1,247 @@
+// Copyright (C) 2014 Massachusetts Institute of Technology, Lincoln Laboratory
+// License: Boost Software License   See LICENSE.txt for the full license.
+// Authors: Davis E. King (davis.king@ll.mit.edu)
+
+#include <mitie/binary_relation_detector_trainer.h>
+#include <dlib/svm_threaded.h>
+#include <dlib/optimization.h>
+
+using namespace dlib;
+using namespace std;
+
+namespace mitie
+{
+
+// ----------------------------------------------------------------------------------------
+
+    binary_relation_detector_trainer::
+    binary_relation_detector_trainer (
+        const std::string& relation_name_,
+        const named_entity_extractor& ner
+    ) : 
+        tfe(ner.get_total_word_feature_extractor()), 
+        beta(0.1), 
+        num_threads(4),
+        relation_name(relation_name_)
+    {
+    }
+
+// ----------------------------------------------------------------------------------------
+
+    std::string binary_relation_detector_trainer::
+    get_relation_name(
+    ) const
+    {
+        return relation_name;
+    }
+
+// ----------------------------------------------------------------------------------------
+
+    unsigned long binary_relation_detector_trainer::
+    num_positive_examples(
+    ) const
+    {
+        return pos_sentences.size();
+    }
+
+// ----------------------------------------------------------------------------------------
+
+    unsigned long binary_relation_detector_trainer::
+    num_negative_examples(
+    ) const
+    {
+        return neg_sentences.size();
+    }
+
+// ----------------------------------------------------------------------------------------
+
+    void binary_relation_detector_trainer::
+    add_positive_binary_relation (
+        const std::vector<std::string>& tokens,
+        unsigned long arg1_start,
+        unsigned long arg1_length,
+        unsigned long arg2_start,
+        unsigned long arg2_length
+    )
+    {
+        DLIB_CASSERT(arg1_length > 0 && arg2_length > 0, "Invalid Inputs");
+        DLIB_CASSERT(mitie_entities_overlap(arg1_start,arg1_length,arg2_start,arg2_length) == 0, 
+            "Binary relation arguments can't overlap.");
+        DLIB_CASSERT(arg1_start+arg1_length <= tokens.size() && arg2_start+arg2_length <= tokens.size(), 
+            "Invalid Inputs");
+
+        pos_sentences.push_back(tokens);
+        pos_arg1s.push_back(std::make_pair(arg1_start, arg1_start+arg1_length));
+        pos_arg2s.push_back(std::make_pair(arg2_start, arg2_start+arg2_length));
+    }
+
+// ----------------------------------------------------------------------------------------
+
+    void binary_relation_detector_trainer::
+    add_negative_binary_relation (
+        const std::vector<std::string>& tokens,
+        unsigned long arg1_start,
+        unsigned long arg1_length,
+        unsigned long arg2_start,
+        unsigned long arg2_length
+    )
+    {
+        DLIB_CASSERT(arg1_length > 0 && arg2_length > 0, "Invalid Inputs");
+        DLIB_CASSERT(mitie_entities_overlap(arg1_start,arg1_length,arg2_start,arg2_length) == 0, 
+            "Binary relation arguments can't overlap.");
+        DLIB_CASSERT(arg1_start+arg1_length <= tokens.size() && arg2_start+arg2_length <= tokens.size(), 
+            "Invalid Inputs");
+
+        neg_sentences.push_back(tokens);
+        neg_arg1s.push_back(std::make_pair(arg1_start, arg1_start+arg1_length));
+        neg_arg2s.push_back(std::make_pair(arg2_start, arg2_start+arg2_length));
+    }
+
+// ----------------------------------------------------------------------------------------
+
+    unsigned long binary_relation_detector_trainer::
+    get_num_threads (
+    ) const
+    {
+        return num_threads;
+    }
+
+// ----------------------------------------------------------------------------------------
+
+    void binary_relation_detector_trainer::
+    set_num_threads (
+        unsigned long num
+    )
+    {
+        num_threads = num;
+    }
+
+// ----------------------------------------------------------------------------------------
+
+    double binary_relation_detector_trainer::
+    get_beta (
+    ) const
+    {
+        return beta;
+    }
+
+// ----------------------------------------------------------------------------------------
+
+    void binary_relation_detector_trainer::
+    set_beta (
+        double new_beta
+    )
+    {
+        DLIB_CASSERT(new_beta >= 0, "Invalid beta");
+        beta = new_beta;
+    }
+
+// ----------------------------------------------------------------------------------------
+
+    class brdt_cv_objective
+    {
+    public:
+        brdt_cv_objective(
+            const unsigned long num_threads_,
+            const int cv_folds_,
+            const double beta_,
+            const std::vector<sparse_vector_type>& samples_,
+            const std::vector<double>& labels_
+        ) : num_threads(num_threads_), cv_folds(cv_folds_), beta(beta_), samples(samples_), labels(labels_) {}
+
+        double operator()(matrix<double,2,1> params) const
+        {
+            params = exp(params);
+            svm_c_linear_dcd_trainer<sparse_linear_kernel<sparse_vector_type> > trainer;
+            trainer.set_c_class1(params(0));
+            trainer.set_c_class2(params(1));
+            cout << "testing with params: " << trans(params);
+            matrix<double> res = cross_validate_trainer_threaded(trainer, samples, labels, cv_folds, num_threads);
+            cout << "cv: "<< res;
+
+            const double fscore = (1+beta*beta) * res(0)*res(1) / (beta*beta*res(1) + res(0));
+
+            cout << "fscore: "<< fscore << endl << endl;
+            return fscore;
+        }
+
+    private:
+        const unsigned long num_threads;
+        const int cv_folds;
+        const double beta;
+        const std::vector<sparse_vector_type>& samples;
+        const std::vector<double>& labels;
+    };
+
+    binary_relation_detector binary_relation_detector_trainer::
+    train (
+    ) const
+    {
+        DLIB_CASSERT(num_positive_examples() > 0, "Not enough training data given.");
+        DLIB_CASSERT(num_negative_examples() > 0, "Not enough training data given.");
+
+        std::vector<sparse_vector_type> samples;
+        std::vector<double> labels;
+
+        for (unsigned long i = 0; i < pos_sentences.size(); ++i)
+        {
+            samples.push_back(extract_binary_relation(pos_sentences[i], pos_arg1s[i], pos_arg2s[i], tfe).feats);
+            labels.push_back(+1);
+
+            // Add the reverse as a negative example since argument ordering matters
+            samples.push_back(extract_binary_relation(pos_sentences[i], pos_arg2s[i], pos_arg1s[i], tfe).feats);
+            labels.push_back(-1);
+        }
+        for (unsigned long i = 0; i < neg_sentences.size(); ++i)
+        {
+            samples.push_back(extract_binary_relation(neg_sentences[i], neg_arg1s[i], neg_arg2s[i], tfe).feats);
+            labels.push_back(-1);
+        }
+
+        randomize_samples(samples, labels);
+
+        const int cv_folds = 6;
+        brdt_cv_objective obj(num_threads, cv_folds, beta, samples, labels);
+
+        matrix<double,2,1> params;
+        params = 5000.0/samples.size(), 5000.0/samples.size();
+        // We do the parameter search in log space.
+        params = log(params);
+        // can't do the parameter search if we don't have enough data.   So if we don't
+        // have much data then just use the default parameters.
+        if (pos_sentences.size() > (unsigned)cv_folds)
+        {
+            matrix<double,2,1> lower_params, upper_params;
+            lower_params = 1.0/samples.size(), 1.0/samples.size();
+            upper_params = 100000.0/samples.size(), 100000.0/samples.size();
+            lower_params = log(lower_params);
+            upper_params = log(upper_params);
+            const double rho_begin = min(upper_params-lower_params)*0.15;
+            const double rho_end = log(1.2/samples.size()) - log(1.0/samples.size());
+            find_max_bobyqa(obj, params, params.size()*2+1, lower_params, upper_params, rho_begin, rho_end, 200);
+        }
+
+
+        // Note that we rescale the parameters to account for the fact that the cross
+        // validation was done on a dataset slightly smaller than the one we ultimately train
+        // on and the C parameters of this trainer are not normalized by the number of training
+        // samples.
+        params = exp(params) * (cv_folds-1.0)/cv_folds;
+        svm_c_linear_dcd_trainer<sparse_linear_kernel<sparse_vector_type> > trainer;
+        trainer.set_c_class1(params(0));
+        trainer.set_c_class2(params(1));
+        cout << "using parameters of: " << trans(params);
+        cout << "now doing training..." << endl;
+        binary_relation_detector bd;
+        bd.df = trainer.train(samples, labels);
+        bd.relation_type = relation_name;
+        bd.total_word_feature_extractor_fingerprint = tfe.get_fingerprint();
+
+        cout << "test on train: " << test_binary_decision_function(bd.df, samples, labels) << endl;
+        return bd;
+    }
+
+// ----------------------------------------------------------------------------------------
+
+}
+
