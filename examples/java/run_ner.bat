56a31fe19d5c8069b1ddfb4f2a9657235f824869
Davis E. King <davis.king@ll.mit.edu>
2014-10-14 14:20:50 -0400

    Added a bat file for running the java examples on windows

---
+++ examples/java/run_ner.bat
@@ -0,0 +1,10 @@
+setlocal
+set PATH=../../mitielib;%PATH%
+set CLASSPATH=../../mitielib/javamitie.jar;. 
+
+
+javac ner.java
+
+java ner ../../sample_text.txt  ../../MITIE-models/english/ner_model.dat ../../MITIE-models/english/binary_relations/rel_classifier_people.person.place_of_birth.svm
+
+pause
