e00f73ca97749df6847d5cbbe9ae71da32c186e9
Davis E. King <davis.king@ll.mit.edu>
2014-10-24 15:42:41 -0400

    Removed cruft

--- examples/java/run_ner.bat
+++ examples/java/run_ner.bat
@@ -5,6 +5,6 @@ set CLASSPATH=../../mitielib/javamitie.jar;.
 
 javac NerExample.java
 
-java NerExample ../../sample_text.txt  ../../MITIE-models/english/ner_model.dat ../../MITIE-models/english/binary_relations/rel_classifier_people.person.place_of_birth.svm
+java NerExample 
 
 pause
--- examples/java/run_ner.sh
+++ examples/java/run_ner.sh
@@ -3,4 +3,4 @@ export CLASSPATH=../../mitielib/javamitie.jar:.
 
 javac NerExample.java
 
-java NerExample ../../sample_text.txt  ../../MITIE-models/english/ner_model.dat ../../MITIE-models/english/binary_relations/rel_classifier_people.person.place_of_birth.svm
+java NerExample 
