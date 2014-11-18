9a0a6063b2026a4ccc08503d5fb47f5841d2cf92
Davis E. King <davis.king@ll.mit.edu>
2014-11-18 09:21:20 -0500

    updated matlab example

---
+++ examples/matlab/extract_entities.cpp
@@ -0,0 +1,54 @@
+
+#include <dlib/matrix.h>
+#include <mitie/named_entity_extractor.h>
+#include <string>
+
+using namespace dlib;
+using namespace std;
+using namespace mitie;
+
+
+#define ARG_4_DEFAULT ""
+
+string classname;
+named_entity_extractor ner;
+bool did_init = false;
+
+void mex_function (
+    const std::vector<std::string>& tokens,
+    std::vector<matrix<double,1> >& entities,
+    std::vector<std::string>& labels,
+    const std::string& model_filename
+) 
+{
+    if (!did_init)
+    {
+        if (model_filename == "")
+            throw dlib::error("You need to give a MITIE ner model file as the second argument the first time you call this function.");
+
+        dlib::deserialize(model_filename) >> classname >> ner;
+        did_init = true;
+    }
+
+    std::vector<pair<unsigned long, unsigned long> > chunks;
+    std::vector<unsigned long> chunk_tags;
+    // Now detect all the entities in the tokens.  The output of this function is a set of
+    // "chunks" of tokens, each a named entity.
+    ner(tokens, chunks, chunk_tags);
+
+    const std::vector<string> tagstr = ner.get_tag_name_strings();
+    entities.clear();
+    labels.clear();
+    // copy the entities into the output variables
+    for (unsigned int i = 0; i < chunks.size(); ++i)
+    {
+        labels.push_back(tagstr[chunk_tags[i]]);
+        const unsigned long length = chunks[i].second-chunks[i].first;
+        entities.push_back(linspace(chunks[i].first+1, chunks[i].second, length));
+    }
+}
+
+
+
+#include "mex_wrapper.cpp"
+
---
+++ examples/matlab/ner_example.m
@@ -0,0 +1,33 @@
+% This example shows how to use the MITIE MATLAB API to perform named entity 
+% recognition.  Note that you must compile the MATLAB API before you can
+% use it.  To do this, execute the following commands:
+%   cd examples/matlab
+%   mkdir build
+%   cmake -G "Visual Studio 10 Win64" ..
+%   cmake --build . --config release --target install
+% That will compile everything and copy the build outputs into the 
+% examples/matlab folder.  You can then run this matlab script.  Note that
+% you will need to change the argument to the -G option to match your
+% compiler if you don't have Visual Studio 10.
+
+
+% Load a text file as a cell array of words
+tokens = tokenize_file('..\..\sample_text.txt')
+
+% Now ask MITIE to identify all the entities in tokens.  Note that we need
+% to supply a ner model file.  Additionally, the model file is loaded on
+% the first call to extract_entities() and then kept forever.  So if you
+% want to load a different model file within the same MATLAB instance you
+% need to do "clear mex" to unload the MITIE mex files.  Then the next call
+% to extract_entities() will load whatever model file you supply.
+[entities,labels] = extract_entities(tokens, '../../MITIE-models/english/ner_model.dat')
+
+disp(['num entities found: ' num2str(numel(entities))])
+
+% Now print out all the entities, in each iteration, we print the entity
+% label and then the text of the entity itself.  Note that entities{i}
+% gives the range in tokens corresponding to the i-th entity.
+for i = 1:numel(entities)
+    disp([labels{i} {tokens{entities{i}}}])        
+end
+
