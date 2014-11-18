09755a00d4022e6a79fae48bfeabc1a10570b416
Davis E. King <davis.king@ll.mit.edu>
2014-11-18 09:22:12 -0500

    updated examples

---
+++ examples/matlab/tokenize_file.cpp
@@ -0,0 +1,35 @@
+
+#include <dlib/matrix.h>
+#include <fstream>
+#include <string>
+#include <mitie/conll_tokenizer.h>
+using namespace dlib;
+using namespace std;
+using namespace mitie;
+
+
+
+void mex_function (
+    const std::string& filename,
+    std::vector<std::string>& tokens
+) 
+{
+    tokens.clear();
+    ifstream fin(filename.c_str());
+    if (!fin)
+        throw error("Unable to load input text file: " + filename);
+
+    // The conll_tokenizer splits the contents of an istream into a bunch of words and is
+    // MITIE's default tokenization method. 
+    conll_tokenizer tok(fin);
+    string token;
+    // Read the tokens out of the file one at a time and store into tokens.
+    while(tok(token))
+        tokens.push_back(token);
+
+}
+
+
+// #including this brings in all the mex boiler plate needed by MATLAB.
+#include "mex_wrapper.cpp"
+
