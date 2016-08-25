715be49a7c6c3c7af3984eab4d539eb6717dd0d0
Alan Nichol <amn41@cam.ac.uk>
2016-08-25 12:04:00 +0100

    updated comments in examples

--- examples/python/categorize_text.py
+++ examples/python/categorize_text.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python
 #
-#    This example shows how to use the MITIE Python API to train a named_entity_extractor.
+#    This example shows how to use the MITIE Python API to use a text_categorizer.
 #
 #
 import sys, os
-import ipdb
+
+
 # Make sure you put the mitielib folder into the python search path.  There are
 # a lot of ways to do this, here we do it programmatically with the following
 # two statements:
-#parent = os.path.dirname(os.path.realpath(__file__))
-#sys.path.append(parent + '/../../mitielib')
+parent = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(parent + '/../../mitielib')
 
 from mitie import *
 
 test_tokens = ["What","a","black","and","bad","day"]
 test_tokens_2 = ["I","am","so","happy"]
 
-# This function does the work of training.  Note that it can take a long time to run
-# when using larger training datasets.  So be patient.
+# load a pre-trained text categorizer
 cat = text_categorizer("new_text_categorizer.dat")
 
+# call the categorizer with a list of tokens, the response is a label (a string)
+# and a score (a number) indicating the confidence of the categorizer
 label, score = cat(test_tokens)
 print(label,score)
 
