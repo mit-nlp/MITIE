97f82e79f7d74e4e07eb625439149682034732bf
Davis King <davis@dlib.net>
2018-04-14 10:19:47 -0400

    Add support for windows to setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,53 @@
 import os.path
 from distutils.core import setup
 from distutils.command.build import build
+import os
+import platform
+import sys
 
 import subprocess
 
 
 class BuildMITIE(build):
     def run(self):
-        subprocess.check_call(['make', 'mitielib'])
-        build.run(self)
-        self.copy_file(
-            'mitielib/libmitie.so',
-             os.path.join(self.build_lib, 'mitie/libmitie.so'))
+        if platform.system() == "Windows":
+            if LooseVersion(self.get_cmake_version()) < '3.1.0':
+                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
+
+            if not os.path.exists('mitielib/build'):
+                os.makedirs('mitielib/build')
+            os.chdir('mitielib/build')
+            if sys.maxsize > 2**32:
+                subprocess.check_call(['cmake', '..', '-A', 'x64'])
+            else:
+                subprocess.check_call(['cmake', '..'])
+
+            subprocess.check_call(['cmake', '--build', '.', '--config', 'Release', '--target', 'install'])
+            os.chdir('../..')
+
+            build.run(self)
+        
+            self.copy_file(
+                'mitielib/mitie.dll',
+                os.path.join(self.build_lib, 'mitie/mitie.dll'))
+        else:
+            subprocess.check_call(['make', 'mitielib'])
+            build.run(self)
+            self.copy_file(
+                'mitielib/libmitie.so',
+                os.path.join(self.build_lib, 'mitie/libmitie.so'))
+
+    def get_cmake_version(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError("CMake must be installed to build the following extensions: " +
+                               ", ".join(e.name for e in self.extensions))
+        return re.search(r'version\s*([\d.]+)', out.decode()).group(1)
 
 
 setup(
-    version='0.2.0',
+    version='0.5.0',
     name='mitie',
     packages=['mitie'],
     package_dir={'mitie': 'mitielib'},
